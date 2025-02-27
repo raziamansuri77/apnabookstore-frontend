0eed348f5ee705ce4f20d785b5bd84abf750f481
razia <raziamansuri77@gmail.com>
2025-02-27 21:41:26 +0530

    testing

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -23,6 +23,7 @@ export default function CartPage() {
       <div className="fixed top-0 left-0 right-0 z-50 bg-white w-full">
         <BeforeNavbar />
         <div className="pt-[35px]">
+          {/* seacrhNavbar */}
           <SearchNavbar />
         </div>
         <Navbar />
